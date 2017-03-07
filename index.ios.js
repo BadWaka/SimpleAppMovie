b40f40761c77b99824eee4f86543791e38ca3ecd
BadWaka <1456683844@qq.com>
2017-03-07 19:01:56 +0800

    后端爬豆瓣新片榜完成

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,88 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    Image,
+    StyleSheet,
+    Text,
+    View
 } from 'react-native';
 
-export default class SimpleAppMovie extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+let MOCKED_MOVIES_DATA = [
+    {
+        title: '标题',
+        year: '2015',
+        posters: {thumbnail: 'https://cdn.pixabay.com/photo/2017/01/07/20/38/portrait-1961529__340.jpg'}
+    },
+];
 
+let host = 'http://localhost:3000'
+
+// 样式
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f5fcff'
+    },
+    thumbnail: {
+        width: 53,
+        height: 81
+    },
+    rightContainer: {
+        flex: 1
+    },
+    title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: 'center'
+    },
+    year: {
+        textAlign: 'center'
+    }
 });
+
+export default class SimpleAppMovie extends Component {
+    constructor(props) {
+        console.log('constructor');
+        super(props);
+        // 初始化状态
+        this.state = {
+            movies: null
+        };
+        // // ES6中需要手动绑定this
+        // this.getMoviesFromApi = this.getMoviesFromApi.bind(this);
+    }
+
+    componentDidMount() {
+        this.getMoviesFromApi();
+    }
+
+    render() {
+        var movie = MOCKED_MOVIES_DATA[0];
+        return (
+            <View style={styles.container}>
+                <Image style={styles.thumbnail} source={{uri: movie.posters.thumbnail}}/>
+                <View style={styles.rightContainer}>
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <Text style={styles.year}>{movie.year}</Text>
+                </View>
+            </View>
+        );
+    }
+
+    // 从Api获取电影数据
+    async getMoviesFromApi() {
+        try {
+            let response = await fetch(host + '/');
+            let responseJson = await response.json();
+            console.log('responseJson = ' + JSON.stringify(responseJson));
+            return responseJson.movies;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
 
 AppRegistry.registerComponent('SimpleAppMovie', () => SimpleAppMovie);
---
+++ server/server.js
@@ -0,0 +1,38 @@
+/**
+ * Created by BadWaka on 07/03/2017.
+ */
+const express = require('express');
+const request = require('request'); // 引入异步请求库request
+const cheerio = require('cheerio'); // 引入cheerio操作DOM
+
+const app = express();
+
+app.listen(3000, function () {
+    console.log('listening 3000...');
+});
+
+app.get('/', function (req, res) {
+
+    request('https://movie.douban.com/chart', function (err, response, body) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        let movies = [];
+        let $ = cheerio.load(body);
+        let items = $('.indent .item');
+        items.each(function (index, item) {
+            let $item = $(item);
+            let movie = {};
+            movie.movieDetailHref = $item.find('.nbg').attr('href');  // 电影详情链接
+            movie.imgThumbnail = $item.find('img').attr('src');   // 缩略图
+            movie.name = $item.find('.pl2').find('a').text().replace(/\s/g, '');    // 电影名字
+            movie.intro = $item.find('.pl2').find('.pl').text(); // 简介
+            movie.rating = $item.find('.pl2').find('.rating_nums').text();  // 等级
+            movie.evaluationCounts = $item.find('.pl2').children('.star').children('.pl').text(); // 评价人数
+            movies.push(movie);
+        });
+        // console.log(movies);
+        res.json(movies);
+    });
+});
